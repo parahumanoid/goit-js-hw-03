e52db921082cc7f280fa28c54e4b276f1d4a809e
ALEX PARSHIKOV <parahumanoid2@gmail.com>
2024-06-20 16:37:50 +0300

    initial commit

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,12 @@
-function checkForSpam(message) {
-    let testline = message.toLowerCase();
-    return (testline.includes("sale") || testline.includes("spam")) ? true : false;
+function filterArray(numbers, value) {
+    let result = new Array();
+    for (currentValue of numbers) {
+        if (currentValue > value) result.push(currentValue);
+    }
+    return result;
 }
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
